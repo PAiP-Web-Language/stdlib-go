dfab6d4b9505c621406757ea85a8c7ff42934017
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2024-10-19 05:54:50 +0200

    Add Tuples

---
+++ base/tuple.go
@@ -0,0 +1,1120 @@
+package base
+
+// Tuple Type
+type Tuple1[T1 any] struct {
+	D1 T1
+}
+
+type Tuple2[
+	T1 any,
+	T2 any,
+] struct {
+	D1 T1
+	D2 T2
+}
+
+type Tuple3[
+	T1 any,
+	T2 any,
+	T3 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+}
+
+type Tuple4[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+}
+
+type Tuple5[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+}
+
+type Tuple6[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+}
+
+type Tuple7[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+}
+
+type Tuple8[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+	T8 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+	D8 T8
+}
+
+type Tuple9[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+	T8 any,
+	T9 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+	D8 T8
+	D9 T9
+}
+
+type Tuple10[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+	T8 any,
+	T9 any,
+	T10 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+	D8 T8
+	D9 T9
+	D10 T10
+}
+
+type Tuple11[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+	T8 any,
+	T9 any,
+	T10 any,
+	T11 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+	D8 T8
+	D9 T9
+	D10 T10
+	D11 T11
+}
+
+type Tuple12[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+	T8 any,
+	T9 any,
+	T10 any,
+	T11 any,
+	T12 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+	D8 T8
+	D9 T9
+	D10 T10
+	D11 T11
+	D12 T12
+}
+
+type Tuple13[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+	T8 any,
+	T9 any,
+	T10 any,
+	T11 any,
+	T12 any,
+	T13 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+	D8 T8
+	D9 T9
+	D10 T10
+	D11 T11
+	D12 T12
+	D13 T13
+}
+
+type Tuple14[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+	T8 any,
+	T9 any,
+	T10 any,
+	T11 any,
+	T12 any,
+	T13 any,
+	T14 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+	D8 T8
+	D9 T9
+	D10 T10
+	D11 T11
+	D12 T12
+	D13 T13
+	D14 T14
+}
+
+type Tuple15[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+	T8 any,
+	T9 any,
+	T10 any,
+	T11 any,
+	T12 any,
+	T13 any,
+	T14 any,
+	T15 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+	D8 T8
+	D9 T9
+	D10 T10
+	D11 T11
+	D12 T12
+	D13 T13
+	D14 T14
+	D15 T15
+}
+
+type Tuple16[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+	T8 any,
+	T9 any,
+	T10 any,
+	T11 any,
+	T12 any,
+	T13 any,
+	T14 any,
+	T15 any,
+	T16 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+	D8 T8
+	D9 T9
+	D10 T10
+	D11 T11
+	D12 T12
+	D13 T13
+	D14 T14
+	D15 T15
+	D16 T16
+}
+
+type Tuple17[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+	T8 any,
+	T9 any,
+	T10 any,
+	T11 any,
+	T12 any,
+	T13 any,
+	T14 any,
+	T15 any,
+	T16 any,
+	T17 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+	D8 T8
+	D9 T9
+	D10 T10
+	D11 T11
+	D12 T12
+	D13 T13
+	D14 T14
+	D15 T15
+	D16 T16
+	D17 T17
+}
+
+type Tuple18[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+	T8 any,
+	T9 any,
+	T10 any,
+	T11 any,
+	T12 any,
+	T13 any,
+	T14 any,
+	T15 any,
+	T16 any,
+	T17 any,
+	T18 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+	D8 T8
+	D9 T9
+	D10 T10
+	D11 T11
+	D12 T12
+	D13 T13
+	D14 T14
+	D15 T15
+	D16 T16
+	D17 T17
+	D18 T18
+}
+
+type Tuple19[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+	T8 any,
+	T9 any,
+	T10 any,
+	T11 any,
+	T12 any,
+	T13 any,
+	T14 any,
+	T15 any,
+	T16 any,
+	T17 any,
+	T18 any,
+	T19 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+	D8 T8
+	D9 T9
+	D10 T10
+	D11 T11
+	D12 T12
+	D13 T13
+	D14 T14
+	D15 T15
+	D16 T16
+	D17 T17
+	D18 T18
+	D19 T19
+}
+
+type Tuple20[
+	T1 any,
+	T2 any,
+	T3 any,
+	T4 any,
+	T5 any,
+	T6 any,
+	T7 any,
+	T8 any,
+	T9 any,
+	T10 any,
+	T11 any,
+	T12 any,
+	T13 any,
+	T14 any,
+	T15 any,
+	T16 any,
+	T17 any,
+	T18 any,
+	T19 any,
+	T20 any,
+] struct {
+	D1 T1
+	D2 T2
+	D3 T3
+	D4 T4
+	D5 T5
+	D6 T6
+	D7 T7
+	D8 T8
+	D9 T9
+	D10 T10
+	D11 T11
+	D12 T12
+	D13 T13
+	D14 T14
+	D15 T15
+	D16 T16
+	D17 T17
+	D18 T18
+	D19 T19
+	D20 T20
+}
+
+func FromTuple1(
+	o1 T1,
+) Tuple1[T1] {
+	return Tuple1{
+		D1: o1,
+	}
+}
+
+func FromTuple2(
+	o1 T1,
+	o2 T2,
+) Tuple2[T1, T2] {
+	return Tuple2{
+		D1: o1,
+		D2: o2,
+	}
+}
+
+func FromTuple3(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+) Tuple3[T1, T2, T3] {
+	return Tuple3{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+	}
+}
+
+func FromTuple4(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+) Tuple4[T1, T2, T3, T4] {
+	return Tuple4{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+	}
+}
+
+func FromTuple5(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+) Tuple5[T1, T2, T3, T4, T5] {
+	return Tuple5{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+	}
+}
+
+func FromTuple6(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+) Tuple6[T1, T2, T3, T4, T5, T6] {
+	return Tuple6{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+	}
+}
+
+func FromTuple7(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+) Tuple7[T1, T2, T3, T4, T5, T6, T7] {
+	return Tuple7{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+	}
+}
+
+func FromTuple8(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+	o8 T8,
+) Tuple8[T1, T2, T3, T4, T5, T6, T7, T8] {
+	return Tuple8{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+		D8: o8,
+	}
+}
+
+func FromTuple9(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+	o8 T8,
+	o9 T9,
+) Tuple9[T1, T2, T3, T4, T5, T6, T7, T8, T9] {
+	return Tuple9{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+		D8: o8,
+		D9: o9,
+	}
+}
+
+func FromTuple10(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+	o8 T8,
+	o9 T9,
+	o10 T10,
+) Tuple10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10] {
+	return Tuple10{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+		D8: o8,
+		D9: o9,
+		D10: o10,
+	}
+}
+
+func FromTuple11(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+	o8 T8,
+	o9 T9,
+	o10 T10,
+	o11 T11,
+) Tuple11[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11] {
+	return Tuple11{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+		D8: o8,
+		D9: o9,
+		D10: o10,
+		D11: o11,
+	}
+}
+
+func FromTuple12(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+	o8 T8,
+	o9 T9,
+	o10 T10,
+	o11 T11,
+	o12 T12,
+) Tuple12[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12] {
+	return Tuple12{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+		D8: o8,
+		D9: o9,
+		D10: o10,
+		D11: o11,
+		D12: o12,
+	}
+}
+
+func FromTuple13(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+	o8 T8,
+	o9 T9,
+	o10 T10,
+	o11 T11,
+	o12 T12,
+	o13 T13,
+) Tuple13[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13] {
+	return Tuple13{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+		D8: o8,
+		D9: o9,
+		D10: o10,
+		D11: o11,
+		D12: o12,
+		D13: o13,
+	}
+}
+
+func FromTuple14(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+	o8 T8,
+	o9 T9,
+	o10 T10,
+	o11 T11,
+	o12 T12,
+	o13 T13,
+	o14 T14,
+) Tuple14[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14] {
+	return Tuple14{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+		D8: o8,
+		D9: o9,
+		D10: o10,
+		D11: o11,
+		D12: o12,
+		D13: o13,
+		D14: o14,
+	}
+}
+
+func FromTuple15(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+	o8 T8,
+	o9 T9,
+	o10 T10,
+	o11 T11,
+	o12 T12,
+	o13 T13,
+	o14 T14,
+	o15 T15,
+) Tuple15[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15] {
+	return Tuple15{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+		D8: o8,
+		D9: o9,
+		D10: o10,
+		D11: o11,
+		D12: o12,
+		D13: o13,
+		D14: o14,
+		D15: o15,
+	}
+}
+
+func FromTuple16(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+	o8 T8,
+	o9 T9,
+	o10 T10,
+	o11 T11,
+	o12 T12,
+	o13 T13,
+	o14 T14,
+	o15 T15,
+	o16 T16,
+) Tuple16[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16] {
+	return Tuple16{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+		D8: o8,
+		D9: o9,
+		D10: o10,
+		D11: o11,
+		D12: o12,
+		D13: o13,
+		D14: o14,
+		D15: o15,
+		D16: o16,
+	}
+}
+
+func FromTuple17(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+	o8 T8,
+	o9 T9,
+	o10 T10,
+	o11 T11,
+	o12 T12,
+	o13 T13,
+	o14 T14,
+	o15 T15,
+	o16 T16,
+	o17 T17,
+) Tuple17[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17] {
+	return Tuple17{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+		D8: o8,
+		D9: o9,
+		D10: o10,
+		D11: o11,
+		D12: o12,
+		D13: o13,
+		D14: o14,
+		D15: o15,
+		D16: o16,
+		D17: o17,
+	}
+}
+
+func FromTuple18(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+	o8 T8,
+	o9 T9,
+	o10 T10,
+	o11 T11,
+	o12 T12,
+	o13 T13,
+	o14 T14,
+	o15 T15,
+	o16 T16,
+	o17 T17,
+	o18 T18,
+) Tuple18[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18] {
+	return Tuple18{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+		D8: o8,
+		D9: o9,
+		D10: o10,
+		D11: o11,
+		D12: o12,
+		D13: o13,
+		D14: o14,
+		D15: o15,
+		D16: o16,
+		D17: o17,
+		D18: o18,
+	}
+}
+
+func FromTuple19(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+	o8 T8,
+	o9 T9,
+	o10 T10,
+	o11 T11,
+	o12 T12,
+	o13 T13,
+	o14 T14,
+	o15 T15,
+	o16 T16,
+	o17 T17,
+	o18 T18,
+	o19 T19,
+) Tuple19[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19] {
+	return Tuple19{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+		D8: o8,
+		D9: o9,
+		D10: o10,
+		D11: o11,
+		D12: o12,
+		D13: o13,
+		D14: o14,
+		D15: o15,
+		D16: o16,
+		D17: o17,
+		D18: o18,
+		D19: o19,
+	}
+}
+
+func FromTuple20(
+	o1 T1,
+	o2 T2,
+	o3 T3,
+	o4 T4,
+	o5 T5,
+	o6 T6,
+	o7 T7,
+	o8 T8,
+	o9 T9,
+	o10 T10,
+	o11 T11,
+	o12 T12,
+	o13 T13,
+	o14 T14,
+	o15 T15,
+	o16 T16,
+	o17 T17,
+	o18 T18,
+	o19 T19,
+	o20 T20
+) Tuple20[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20] {
+	return Tuple20{
+		D1: o1,
+		D2: o2,
+		D3: o3,
+		D4: o4,
+		D5: o5,
+		D6: o6,
+		D7: o7,
+		D8: o8,
+		D9: o9,
+		D10: o10,
+		D11: o11,
+		D12: o12,
+		D13: o13,
+		D14: o14,
+		D15: o15,
+		D16: o16,
+		D17: o17,
+		D18: o18,
+		D19: o19,
+		D20: o20,
+	}
+}
+
+func ToTuple1(t Tuple1[T1]) (T1) {
+	return t.D1
+}
+
+func ToTuple2(t Tuple2[T1, T2]) (T1, T2) {
+	return t.D1, t.D2
+}
+
+func ToTuple3(t Tuple3[T1, T2, T3]) (T1, T2, T3) {
+	return t.D1, t.D2, t.D3
+}
+
+func ToTuple4(t Tuple4[T1, T2, T3, T4]) (T1, T2, T3, T4) {
+	return t.D1, t.D2, t.D3, t.D4
+}
+
+func ToTuple5(t Tuple5[T1, T2, T3, T4, T5]) (T1, T2, T3, T4, T5) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5
+}
+
+func ToTuple6(t Tuple6[T1, T2, T3, T4, T5, T6]) (T1, T2, T3, T4, T5, T6) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6
+}
+
+func ToTuple7(t Tuple7[T1, T2, T3, T4, T5, T6, T7]) (T1, T2, T3, T4, T5, T6, T7) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7
+}
+
+func ToTuple8(t Tuple8[T1, T2, T3, T4, T5, T6, T7, T8]) (T1, T2, T3, T4, T5, T6, T7, T8) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7, t.D8
+}
+
+func ToTuple9(t Tuple9[T1, T2, T3, T4, T5, T6, T7, T8, T9]) (T1, T2, T3, T4, T5, T6, T7, T8, T9) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7, t.D8, t.D9
+}
+
+func ToTuple10(t Tuple10[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10]) (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7, t.D8, t.D9, t.D10
+}
+
+func ToTuple11(t Tuple11[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11]) (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7, t.D8, t.D9, t.D10, t.D11
+}
+
+func ToTuple12(t Tuple12[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12]) (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7, t.D8, t.D9, t.D10, t.D11, t.D12
+}
+
+func ToTuple13(t Tuple13[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13]) (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7, t.D8, t.D9, t.D10, t.D11, t.D12, t.D13
+}
+
+func ToTuple14(t Tuple14[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14]) (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7, t.D8, t.D9, t.D10, t.D11, t.D12, t.D13, t.D14
+}
+
+func ToTuple15(t Tuple15[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15]) (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7, t.D8, t.D9, t.D10, t.D11, t.D12, t.D13, t.D14, t.D15
+}
+
+func ToTuple16(t Tuple16[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16]) (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7, t.D8, t.D9, t.D10, t.D11, t.D12, t.D13, t.D14, t.D15, t.D16
+}
+
+func ToTuple17(t Tuple17[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17]) (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7, t.D8, t.D9, t.D10, t.D11, t.D12, t.D13, t.D14, t.D15, t.D16, t.D17
+}
+
+func ToTuple18(t Tuple18[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18]) (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7, t.D8, t.D9, t.D10, t.D11, t.D12, t.D13, t.D14, t.D15, t.D16, t.D17, t.D18
+}
+
+func ToTuple19(t Tuple19[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19]) (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7, t.D8, t.D9, t.D10, t.D11, t.D12, t.D13, t.D14, t.D15, t.D16, t.D17, t.D18, t.D19
+}
+
+func ToTuple20(t Tuple20[T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20]) (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20) {
+	return t.D1, t.D2, t.D3, t.D4, t.D5, t.D6, t.D7, t.D8, t.D9, t.D10, t.D11, t.D12, t.D13, t.D14, t.D15, t.D16, t.D17, t.D18, t.D19, t.D20
+}
