305d21e1551ee4ae52ece15bbdfb52695e1e9ea7
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2024-10-19 03:30:26 +0200

    Added LogicalUnions

---
+++ base/logicalunion.go
@@ -0,0 +1,97 @@
+package base
+
+// LogicalUnion is a type annotation for a union in practice its empty struct
+// This is meant to be only typed annotation that is not checked
+type LogicalUnion struct {}
+type LogicalUnion1[T1 any] struct {}
+type LogicalUnion2[T1 any, T2 any] struct {}
+type LogicalUnion3[T1 any, T2 any, T3 any] struct {}
+type LogicalUnion4[T1 any, T2 any, T3 any, T4 any] struct {}
+type LogicalUnion5[T1 any, T2 any, T3 any, T4 any, T5 any] struct {}
+type LogicalUnion6[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any] struct {}
+type LogicalUnion7[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any] struct {}
+type LogicalUnion8[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any] struct {}
+type LogicalUnion9[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any] struct {}
+type LogicalUnion10[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any] struct {}
+type LogicalUnion11[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any] struct {}
+type LogicalUnion12[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any] struct {}
+type LogicalUnion13[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any] struct {}
+type LogicalUnion14[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any] struct {}
+type LogicalUnion15[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any] struct {}
+type LogicalUnion16[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any] struct {}
+type LogicalUnion17[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any] struct {}
+type LogicalUnion18[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any, T18 any] struct {}
+type LogicalUnion19[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any, T18 any, T19 any] struct {}
+type LogicalUnion20[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any, T18 any, T19 any, T20 any] struct {}
+
+// LogicalUnion is a type annotation for a union in practice its empty struct
+// This is meant to be only typed annotation that is not checked
+type LU struct {}
+type LU1[T1 any] struct {}
+type LU2[T1 any, T2 any] struct {}
+type LU3[T1 any, T2 any, T3 any] struct {}
+type LU4[T1 any, T2 any, T3 any, T4 any] struct {}
+type LU5[T1 any, T2 any, T3 any, T4 any, T5 any] struct {}
+type LU6[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any] struct {}
+type LU7[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any] struct {}
+type LU8[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any] struct {}
+type LU9[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any] struct {}
+type LU10[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any] struct {}
+type LU11[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any] struct {}
+type LU12[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any] struct {}
+type LU13[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any] struct {}
+type LU14[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any] struct {}
+type LU15[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any] struct {}
+type LU16[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any] struct {}
+type LU17[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any] struct {}
+type LU18[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any, T18 any] struct {}
+type LU19[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any, T18 any, T19 any] struct {}
+type LU20[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any, T18 any, T19 any, T20 any] struct {}
+
+// LogicalUnionInterface is a type annotation for a union in practice its empty interface
+// This is meant to be only typed annotation that is not checked
+type LogicalUnionInterface interface {}
+type LogicalUnionInterface1[T1 any] interface {}
+type LogicalUnionInterface2[T1 any, T2 any] interface {}
+type LogicalUnionInterface3[T1 any, T2 any, T3 any] interface {}
+type LogicalUnionInterface4[T1 any, T2 any, T3 any, T4 any] interface {}
+type LogicalUnionInterface5[T1 any, T2 any, T3 any, T4 any, T5 any] interface {}
+type LogicalUnionInterface6[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any] interface {}
+type LogicalUnionInterface7[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any] interface {}
+type LogicalUnionInterface8[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any] interface {}
+type LogicalUnionInterface9[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any] interface {}
+type LogicalUnionInterface10[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any] interface {}
+type LogicalUnionInterface11[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any] interface {}
+type LogicalUnionInterface12[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any] interface {}
+type LogicalUnionInterface13[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any] interface {}
+type LogicalUnionInterface14[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any] interface {}
+type LogicalUnionInterface15[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any] interface {}
+type LogicalUnionInterface16[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any] interface {}
+type LogicalUnionInterface17[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any] interface {}
+type LogicalUnionInterface18[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any, T18 any] interface {}
+type LogicalUnionInterface19[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any, T18 any, T19 any] interface {}
+type LogicalUnionInterface20[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any, T18 any, T19 any, T20 any] interface {}
+
+// LogicalUnionInterface is a type annotation for a union in practice its empty interface
+// This is meant to be only typed annotation that is not checked
+type LUI interface {}
+type LUI1[T1 any] interface {}
+type LUI2[T1 any, T2 any] interface {}
+type LUI3[T1 any, T2 any, T3 any] interface {}
+type LUI4[T1 any, T2 any, T3 any, T4 any] interface {}
+type LUI5[T1 any, T2 any, T3 any, T4 any, T5 any] interface {}
+type LUI6[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any] interface {}
+type LUI7[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any] interface {}
+type LUI8[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any] interface {}
+type LUI9[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any] interface {}
+type LUI10[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any] interface {}
+type LUI11[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any] interface {}
+type LUI12[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any] interface {}
+type LUI13[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any] interface {}
+type LUI14[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any] interface {}
+type LUI15[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any] interface {}
+type LUI16[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any] interface {}
+type LUI17[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any] interface {}
+type LUI18[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any, T18 any] interface {}
+type LUI19[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any, T18 any, T19 any] interface {}
+type LUI20[T1 any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any, T9 any, T10 any, T11 any, T12 any, T13 any, T14 any, T15 any, T16 any, T17 any, T18 any, T19 any, T20 any] interface {}
