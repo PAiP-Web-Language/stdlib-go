cf1aaff9da43304338f2d11622be0ed0cb1852a1
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2024-11-02 06:14:23 +0000

    Fix LogIfFailed

--- test/if_failed.go
+++ test/if_failed.go
@@ -19,7 +19,7 @@ func IfFailed(t testing.TB, f func()) {
 // LogIfFailed is a function that will log message in specified format if test failed
 func LogIfFailed(t testing.TB, format string, args ...any) {
 	t.Helper()
-	msg := fmt.Sprintf(format, args)
+	msg := fmt.Sprintf(format, args...)
 	t.Cleanup(func() {
 		t.Helper()
 		if t.Failed() {
