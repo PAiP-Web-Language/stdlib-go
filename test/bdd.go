aa85db1f9fce55e9c76f2318c36590314f6b360a
Patryk Adamczyk <patrykadamczyk@patrykadamczyk.net>
2024-12-19 06:51:06 +0100

    Fix formatting

--- test/bdd.go
+++ test/bdd.go
@@ -26,7 +26,7 @@ type BDDType struct {
 //      )
 //  )
 // }
-func (b *BDDType)RunFeatureSet(t *testing.T, features ...func(TI)) {
+func (b *BDDType) RunFeatureSet(t *testing.T, features ...func(TI)) {
 	ti := WrapT(t)
 	defer ti.Defer()
 	for _, f := range features {
@@ -36,7 +36,7 @@ func (b *BDDType)RunFeatureSet(t *testing.T, features ...func(TI)) {
 
 // Feature is function that creates new feature to test
 // It takes name and subelements (Like Description, Rule, Given, When etc.)
-func (b *BDDType)Feature(name string, subelements ...func(TI)) func(t TI) {
+func (b *BDDType) Feature(name string, subelements ...func(TI)) func(t TI) {
 	return func(t TI) {
 		t.Helper()
 		t.Run(
@@ -54,15 +54,15 @@ func (b *BDDType)Feature(name string, subelements ...func(TI)) func(t TI) {
 
 // Description is function that is used just to note some additional information
 // This function is meant to be used only to note some detail in tests but it does not do anything
-func (b *BDDType)Description(name string) func(t TI) {
+func (b *BDDType) Description(name string) func(t TI) {
 	return func(t TI) {}
 }
 
-func (b *BDDType)Rule(name string, body func(TI)) func(TI) {
+func (b *BDDType) Rule(name string, body func(TI)) func(TI) {
 	return body
 }
 
-func (b *BDDType)Scenario(name string, subelements ...func(TI)) func(TI) {
+func (b *BDDType) Scenario(name string, subelements ...func(TI)) func(TI) {
 	return func(t TI) {
 		t.Helper()
 		t.Run(
@@ -78,27 +78,27 @@ func (b *BDDType)Scenario(name string, subelements ...func(TI)) func(TI) {
 	}
 }
 
-func (b *BDDType)Given(name string, body func(TI)) func(TI) {
+func (b *BDDType) Given(name string, body func(TI)) func(TI) {
 	return body
 }
 
-func (b *BDDType)When(name string, body func(TI)) func(TI) {
+func (b *BDDType) When(name string, body func(TI)) func(TI) {
 	return body
 }
 
-func (b *BDDType)Then(name string, body func(TI)) func(TI) {
+func (b *BDDType) Then(name string, body func(TI)) func(TI) {
 	return body
 }
 
-func (b *BDDType)And(name string, body func(TI)) func(TI) {
+func (b *BDDType) And(name string, body func(TI)) func(TI) {
 	return body
 }
 
-func (b *BDDType)But(name string, body func(TI)) func(TI) {
+func (b *BDDType) But(name string, body func(TI)) func(TI) {
 	return body
 }
 
-func (b *BDDType)Background(name string, subelements ...func(TI)) func(TI) {
+func (b *BDDType) Background(name string, subelements ...func(TI)) func(TI) {
 	return func(t TI) {
 		t.Helper()
 		t.Run(
@@ -114,14 +114,14 @@ func (b *BDDType)Background(name string, subelements ...func(TI)) func(TI) {
 	}
 }
 
-// func (b *BDDType)ScenarioOutline(name string, body func(TI)) func(TI) {
+// func (b *BDDType) ScenarioOutline(name string, body func(TI)) func(TI) {
 // }
 //
-// func (b *BDDType)ScenarioTemplate(name string, body func(TI)) func(TI) {
+// func (b *BDDType) ScenarioTemplate(name string, body func(TI)) func(TI) {
 // }
 //
-// func (b *BDDType)Examples(name string, body func(TI)) func(TI) {
+// func (b *BDDType) Examples(name string, body func(TI)) func(TI) {
 // }
 //
-// func (b *BDDType)Scenarios(name string, body func(TI)) func(TI) {
+// func (b *BDDType) Scenarios(name string, body func(TI)) func(TI) {
 // }
